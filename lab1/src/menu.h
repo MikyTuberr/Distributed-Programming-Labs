719f47afc303718855f508b3cfdb378a2ef22c36
Piotr Lachowicz <120327639+MikyTuberr@users.noreply.github.com>
2024-03-14 16:03:25 +0100

    Add files via upload

---
+++ lab1/src/menu.h
@@ -0,0 +1,17 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+#include "node.h"
+
+void menu();
+void handle_insert();
+void handle_erase();
+void handle_input();
+void handle_traverse();
+void handle_quit();
+
+#endif // MENU_H
