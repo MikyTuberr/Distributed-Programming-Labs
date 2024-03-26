3c3213eec9a398535cee37d84762048dc6f541cd
Piotr Lachowicz <120327639+MikyTuberr@users.noreply.github.com>
2024-03-26 12:03:43 +0100

    Update script.sh

--- lab3/script.sh
+++ lab3/script.sh
@@ -3,18 +3,13 @@
 commands=(
     "wget -O large_file https://ash-speed.hetzner.com/100MB.bin"
     "ping -c 10 facebook.com"
-    "sleep 5"
+    "sleep 5 && echo ENDED WAITING"
     "openssl speed -elapsed -evp sha256"
 )
 
-execute_command_parallel() {
-    "$@" &
-}
-
 for command in "${commands[@]}"; do
-    execute_command_parallel $command
+    eval "$command" &
 done
 
 wait
-
 echo "ALL CHILD PROCESSES HAVE FINISHED."
